6adac660eae28fe3e9b651bb2db5efc17a4a2e22
Anatoly Gurbanov <tomgurbanov@gmail.com>
2021-02-15 04:34:14 +0300

    make tests for validation

--- ValidationTestProject/ValidationTestProjectTests/ValidationTestProjectTests.swift
+++ ValidationTestProject/ValidationTestProjectTests/ValidationTestProjectTests.swift
@@ -9,6 +9,15 @@ import XCTest
 @testable import ValidationTestProject
 
 class ValidationTestProjectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -17,10 +26,63 @@ class ValidationTestProjectTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    
+    func testEmailValidation() throws {
+        let validator = EmailValidator()
+        let shortEmail = try? validator.validated( "ab")
+        XCTAssertNil(shortEmail)
+        
+        let tooLongEmail = try? validator.validated("ABCDEFGHIJKLMNOPQRSTUVWXY@gmail.com") //36 chars
+        XCTAssertNil(tooLongEmail)
+        
+        let whitespaceEmail = try? validator.validated("a cv as@gmail.com")
+        XCTAssertNil(whitespaceEmail)
+        
+        let specialCharactersEmail = try? validator.validated("a#as!@gmail.com")
+        XCTAssertNil(specialCharactersEmail)
+        
+        let dotStartedEmail = try? validator.validated(".aas@gmail.com")
+        XCTAssertNil(dotStartedEmail)
+        
+        let validMixedWithNumberEmail = "and.123@GMAIL.COM"
+        let validatedNumbersEmail = try? validator.validated(validMixedWithNumberEmail)
+        XCTAssertEqual(validMixedWithNumberEmail, validatedNumbersEmail)
+        
+        let validMixedWithMinusEmail = "and-123@GMAIL.COM"
+        let validatedMinusEmail = try? validator.validated(validMixedWithMinusEmail)
+        XCTAssertEqual(validMixedWithMinusEmail, validatedMinusEmail)
+        
+        let mixedWithNumbers2 = try? validator.validated("123as@gmail.com")
+        XCTAssertNil(mixedWithNumbers2)
+        
+        let validUsername = "validemail@gmail.com"
+        let validatedUsername = try? validator.validated(validUsername)
+        XCTAssertEqual(validUsername, validatedUsername)
+    }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testNicknameValidation() throws {
+        let validator = UserNameValidator()
+        let shortUsername = try? validator.validated( "ab")
+        XCTAssertNil(shortUsername)
+        
+        let tooLongUsername = try? validator.validated("ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFG") //33 chars
+        XCTAssertNil(tooLongUsername)
+        
+        let whitespaceUsername = try? validator.validated("a cv as")
+        XCTAssertNil(whitespaceUsername)
+        
+        let specialCharactersUsername = try? validator.validated("a#as!")
+        XCTAssertNil(specialCharactersUsername)
+        
+        let mixedWithNumbers = try? validator.validated("arlind123")
+        XCTAssertNil(mixedWithNumbers)
+        
+        let mixedWithNumbers2 = try? validator.validated("123arl12ind123")
+        XCTAssertNil(mixedWithNumbers2)
+        
+        let validUsername = "arlind"
+        let validatedUsername = try? validator.validated(validUsername)
+        XCTAssertEqual(validUsername, validatedUsername)
     }
 
     func testPerformanceExample() throws {
